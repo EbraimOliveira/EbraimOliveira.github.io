99a0e5ba89bfce829284c7a2d11efe58eccc7aaa
Ebraim Oliveira <ebraim_oliveira@hotmail.com>
2023-11-25 23:31:43 -0300

    refact projects summary

--- src/utils/languages.tsx
+++ src/utils/languages.tsx
@@ -1,32 +1,47 @@
 const welcomeTxtPt = `Olá, sou Ebraim Oliveira, desenvolvedor web e mobile. É um prazer te receber no meu portfólio !
-Aqui você encontrará alguns dos projetos nos quais trabalhei e informações sobre meus hobbies e experiências como voluntário. Fique à vontade para explorar e acompanhar o meu progresso pessoal e profissional.`
+Aqui você encontrará alguns dos projetos nos quais trabalhei e informações sobre meus hobbies e experiências como voluntário.
+Fique à vontade para explorar e acompanhar o meu progresso pessoal e profissional.`
 
 const welcomeTxtEn = `Hello, I'm Ebraim Oliveira, web and mobile developer. It's a pleasure to welcome you to my portfolio !
-Here you will find some of the projects I've worked on and information about my hobbies and volunteer experiences. Feel free to explore and follow my personal and professional progress.`
+Here you will find some of the projects I've worked on and information about my hobbies and volunteer experiences. 
+Feel free to explore and follow my personal and professional progress.`
 
 const hobbyClimbPt = `Assim como na programação, a capacidade de resolver problemas é essencial na escalada.
- Talvez por isso essa seja a minha atividade de lazer favorita e onde reside a minha verdadeira paixão. Amo o contato com a natureza e as dificuldades que esse esporte me permite superar.`
+ Talvez por isso essa seja a minha atividade de lazer favorita e onde reside a minha verdadeira paixão. 
+ Amo o contato com a natureza e as dificuldades que esse esporte me permite superar.`
 
-const hobbyClimbEn = "Just like in programming, problem-solving skills are crucial in climbing. Perhaps that's why it's my favorite leisure activity and where my true passion resides. I love being in touch with nature and the challenges this sport allows me to overcome."
+const hobbyClimbEn = `Just like in programming, problem-solving skills are crucial in climbing. 
+Perhaps that's why it's my favorite leisure activity and where my true passion resides. 
+I love being in touch with nature and the challenges this sport allows me to overcome.`
 
 const hobbyBoardGamesPt = ''
 const hobbyBoardGamesEn = ''
 
-const aboutPixelArtPt = 'Uma página interativa que possibilita selecionar cores a partir de uma paleta diversificada, alterar o tamanho do grid e exercer toda a criatividade em artes pixeladas'
+const aboutPixelArtPt = `Uma página interativa que possibilita selecionar cores a partir de uma paleta diversificada, 
+alterar o tamanho do grid e exercer toda a criatividade em artes pixeladas`
 
 const aboutPixelArtEn = 'Interactive page that allows selecting colors from a diverse palette, changing the grid size, and unleashing creativity in pixel art.'
 
-const aboutPortfolioPt = 'Um projeto básico mas que apresentou diversos desafios e proporcionou muito aprendizado. Criado totalmente do zero e sem nenhum modelo, optei por utilizar ferramentas como typescript e react que não necessariamente precisavam ser usados mas que me permitiram aprimorar meu conhecimento com as referidas tecnologias'
+const aboutPortfolioPt = `Um projeto básico mas que apresentou diversos desafios e proporcionou muito aprendizado.
+ Criado totalmente do zero e sem nenhum modelo, optei por utilizar ferramentas como typescript e react que não necessariamente
+  precisavam ser usados mas que me permitiram aprimorar meu conhecimento com as referidas tecnologias.`
 
-const aboutPortfolioEn = 'A basic project that posed various challenges and provided significant learning opportunities. Created entirely from scratch without any templates, I chose to use tools like TypeScript and React, which were not strictly necessary but allowed me to enhance my knowledge of these technologies.'
+const aboutPortfolioEn = `A basic project that posed various challenges and provided significant learning opportunities. 
+Created entirely from scratch without any templates, I chose to use tools like TypeScript and React, 
+which were not strictly necessary but allowed me to enhance my knowledge of these technologies.`
 
-const aboutCosmoPt = 'Projeto feito em grupo com outros devs Jrs também em início de carreira, este é eu projeto mais ambicioso até o momento. Construido inteiramente do zero e utilizando tecnologias que nunca havia estudado antes (React Native e frameworks), criamos uma integração entre front e back, aplicamos Arquiteturas em Camadas MSC (Model-Service-Controller), metodos de autenticação, conhecimento da estrutura Dockerfile e docker-compose, banco de dados não relacional (Mongo)...'
+const aboutCosmoPt = `Projeto feito em grupo com outros devs Jrs também em início de carreira, este é eu projeto mais ambicioso até o momento. 
+Construido inteiramente do zero e utilizando tecnologias que nunca havia estudado antes (React Native e frameworks),
+ criamos uma integração entre front e back, aplicamos Arquiteturas em Camadas MSC (Model-Service-Controller), 
+ metodos de autenticação, conhecimento da estrutura Dockerfile e docker-compose, banco de dados não relacional (Mongo)...`
 
 const aboutCosmoEn = ''
 
-const aboutAlgorithmsPt = 'Uma aplicação web simples mas poderosa, que utiliza um exepcional algoritmo de ordenação. Insira um valor e pressione o botão para ver o Merge Sort em ação.'
+const aboutAlgorithmsPt = `Uma aplicação web simples mas poderosa, que utiliza um exepcional algoritmo de ordenação. 
+Insira um valor e pressione o botão para ver o Merge Sort em ação.`
 
-const aboutAlgorithmsEn = 'A simple yet powerful web application that utilizes a robust sorting algorithm. Enter a value and press the button to see the Merge Sort in action.'
+const aboutAlgorithmsEn = `A simple yet powerful web application that utilizes a robust sorting algorithm.
+Enter a value and press the button to see the Merge Sort in action.`
 
 export const portuguese = {
   password: 'senha',
